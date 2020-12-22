e69f80e991414bb6b2a032990b2cebca40208f4d
waqas-ahmd <65waqas@gmail.com>
2020-12-22 22:30:00 +0500

    update

--- functions/sendNotification/sendNotification.js
+++ functions/sendNotification/sendNotification.js
@@ -1,5 +1,19 @@
 var admin = require("firebase-admin");
-var serviceAccount = require("./serviceAccountKey.json");
+
+var serviceAccount = {
+  type: "service_account",
+  project_id: "custom-notification-app",
+  private_key_id: process.env.PRIVATE_KEY_ID,
+  private_key: process.env.PRIVATE_KEY,
+  client_email:
+    "firebase-adminsdk-usgkj@custom-notification-app.iam.gserviceaccount.com",
+  client_id: "106371842359218757591",
+  auth_uri: "https://accounts.google.com/o/oauth2/auth",
+  token_uri: "https://oauth2.googleapis.com/token",
+  auth_provider_x509_cert_url: "https://www.googleapis.com/oauth2/v1/certs",
+  client_x509_cert_url:
+    "https://www.googleapis.com/robot/v1/metadata/x509/firebase-adminsdk-usgkj%40custom-notification-app.iam.gserviceaccount.com",
+};
 
 if (!admin.apps.length) {
   admin.initializeApp({
